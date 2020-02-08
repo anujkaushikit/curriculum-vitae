8a44c25ddb3754a2a1260b43255ce9051b6fac3f
Anuj Kaushik <55687624+anujkaushikit@users.noreply.github.com>
2020-02-08 09:48:58 +0530

    curriculum-vitae
    curriculum-vitae

---
+++ precache-manifest.11d33b7ca094fc7a35031d155ad5735e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d971e43d5a821d4c4ccf521b003e3ac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30110700e3e7300e38cb",
+    "url": "/static/css/main.c24ef35a.chunk.css"
+  },
+  {
+    "revision": "302d516aed15e8b6f4c8",
+    "url": "/static/js/2.ab767dce.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ab767dce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30110700e3e7300e38cb",
+    "url": "/static/js/main.04e3bd6c.chunk.js"
+  },
+  {
+    "revision": "b9821db3055a97b4abdf",
+    "url": "/static/js/runtime-main.b777d170.js"
+  }
+]);
