f57a7421cc34ee70e892004c465b4dd8c5a5b506
Anuj Kaushik <55687624+anujkaushikit@users.noreply.github.com>
2020-02-08 16:24:56 +0530

    cv
    cv

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d27ea53aab3094e9cf485cbf270f205.js"
+  "/precache-manifest.21c98c285d18e35937dacb34431d1940.js"
 );
 
 self.addEventListener('message', (event) => {
